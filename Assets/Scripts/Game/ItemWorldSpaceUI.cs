29f8fb4103d6724ce9f7a0e0f71821723f5907f9
Jackson Dumas <tylerdumas3@hotmail.com>
2020-11-26 18:51:01 -0600

    Added World Space UI
    - added text that sits in world space, which highlights when hovered over
    - added KeyModel assets folder
    - added Inventory.cs
    - added ItemWorldSpaceUI.cs

---
+++ Assets/Scripts/Game/ItemWorldSpaceUI.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemWorldSpaceUI : MonoBehaviour {
+    [SerializeField] private string itemName = "Default";
+    private Text worldSpaceText = null;
+    private bool displayName = false;
+    [SerializeField] private float fadeTime = 0.1f;
+
+    private void Awake() {
+        worldSpaceText = GetComponentInChildren<Text>();
+    }
+
+    void Start(){
+        worldSpaceText.color = Color.clear;
+    }
+
+    private void Update() {
+        FadeText();
+    }
+
+    void FadeText(){
+        if( displayName ){
+            worldSpaceText.text = itemName;
+            worldSpaceText.color = Color.Lerp( worldSpaceText.color, Color.white, fadeTime * Time.deltaTime );
+        }else{
+            worldSpaceText.color = Color.Lerp( worldSpaceText.color, Color.clear, fadeTime * Time.deltaTime );
+        }
+    }
+
+    private void OnMouseOver() {
+        displayName = true;
+    }
+
+    private void OnMouseExit() {
+        displayName = false;
+    }
+}
